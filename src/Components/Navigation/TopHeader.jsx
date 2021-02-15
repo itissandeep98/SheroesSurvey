7ad6d33a22c01d7c39967bdd8bdf9aa2a56bdba8
itissandeep98 <itissandeep98@gmail.com>
2021-02-15 18:49:33 +0530

    icons added

--- src/Components/Navigation/TopHeader.jsx
+++ src/Components/Navigation/TopHeader.jsx
@@ -1,12 +1,16 @@
 import { Col, Container, Row } from 'reactstrap';
+import { Image } from 'semantic-ui-react';
 import './style.css';
 
 function TopHeader() {
 	return (
 		<Container fluid className="navbar_top shadow sticky-top bg-danger py-4">
 			<Row>
-				<Col className="text-center">
-					<h1>SHeroes</h1>
+				<Col className=" justify-content-center d-flex">
+					<Image
+						src={process.env.PUBLIC_URL + '/Icons/logo.svg'}
+						alt="sheroes"
+					/>
 				</Col>
 			</Row>
 		</Container>
