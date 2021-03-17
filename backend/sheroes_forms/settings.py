42c2cdd1293e8c44380870270958b9391f17cf84
Navneet <navneet.agarwal2058@gmail.com>
2021-03-17 23:19:06 +0530

    v0.04

--- backend/sheroes_forms/settings.py
+++ backend/sheroes_forms/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'a5nczn17h#6@g@psrt3argdqi22$%kwk4@2$cvy=4_099&0vpr'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["sheroes-form.herokuapp.com","127.0.0.1"]
+ALLOWED_HOSTS = ["sheroes-form.herokuapp.com","127.0.0.1","0.0.0.0"]
 
 
 # Application definition
@@ -123,6 +123,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
+
 
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL = '/static/'
