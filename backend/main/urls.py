c315e003d702272c2468f950a9cf69bda6d3bdb5
Navneet <navneet.agarwal2058@gmail.com>
2021-03-17 20:54:27 +0530

    heroku v0.02

--- backend/main/urls.py
+++ backend/main/urls.py
@@ -4,5 +4,6 @@ from .api import FormsViewSet, UsersViewSet
 router = routers.DefaultRouter()
 router.register('api/forms', FormsViewSet, 'Forms')
 router.register('api/users', UsersViewSet, 'Users')
+router.register('api/', UsersViewSet, 'Forms')
 
 urlpatterns = router.urls
