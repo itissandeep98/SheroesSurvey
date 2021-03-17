dd597d2dda528e8d3108681fc88ead9eaff85f94
Navneet <navneet.agarwal2058@gmail.com>
2021-03-18 01:58:50 +0530

    v0.04

--- backend/main/urls.py
+++ backend/main/urls.py
@@ -3,7 +3,7 @@ from .api import FormsViewSet, UsersViewSet
 from django.urls import path, include
 
 router = routers.DefaultRouter()
-router.register('api/forms', FormsViewSet, 'Forms')
+router.register('api/forms/', FormsViewSet, 'Forms')
 router.register('api/users', UsersViewSet, 'Users')
 router.register('api/', UsersViewSet, 'Forms')
 
