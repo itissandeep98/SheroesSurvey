ed06f332cbb68c1d12cebd51b40ab7aaadae15ce
Navneet <navneet.agarwal2058@gmail.com>
2021-03-18 00:51:20 +0530

    v0.04

--- backend/main/urls.py
+++ backend/main/urls.py
@@ -4,6 +4,6 @@ from .api import FormsViewSet, UsersViewSet
 router = routers.DefaultRouter()
 router.register('api/forms', FormsViewSet, 'Forms')
 router.register('api/users', UsersViewSet, 'Users')
-router.register('api/', UsersViewSet, 'Forms')
+# router.register('api/', UsersViewSet, 'Forms')
 
 urlpatterns = router.urls
