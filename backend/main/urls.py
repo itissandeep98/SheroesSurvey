c9ddcf41c8f510b77b697831484dc16b0075239d
Navneet <navneet.agarwal2058@gmail.com>
2021-03-18 11:33:47 +0530

    v0.04

--- backend/main/urls.py
+++ backend/main/urls.py
@@ -3,8 +3,8 @@ from .views import (FormsViewSet, UsersViewSet)
 from django.urls import path, include
 
 router = DefaultRouter()
-router.register(r'api/forms', FormsViewSet)
-router.register(r'api/users', UsersViewSet)
-router.register(r'api', UsersViewSet)
+router.register(r'forms', FormsViewSet)
+router.register(r'users', UsersViewSet)
+# router.register(r'', UsersViewSet)
 
 urlpatterns = [path("", include(router.urls)), ]
