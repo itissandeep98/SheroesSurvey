e42ffe9f6315954d0db5ae9154bacf758eb53afc
itissandeep98 <itissandeep98@gmail.com>
2021-04-15 23:51:58 +0530

    redux persist added

--- frontend/src/Store/Reducers/rootReducer.jsx
+++ frontend/src/Store/Reducers/rootReducer.jsx
@@ -3,6 +3,19 @@ import authReducer from './authReducer';
 import formReducer from './formReducer';
 import responseReducer from './responseReducer';
 import userReducer from './userReducer';
+import localForage from 'localforage';
+import { persistReducer } from 'redux-persist';
+import { encryptTransform } from 'redux-persist-transform-encrypt';
+
+const persistConfig = {
+	key: 'root',
+	storage: localForage,
+	transforms: [
+		encryptTransform({
+			secretKey: 'as5a168asd1a8a3fds5g157x687sd',
+		}),
+	],
+};
 
 const rootReducer = combineReducers({
 	form: formReducer,
@@ -11,4 +24,6 @@ const rootReducer = combineReducers({
 	auth: authReducer,
 });
 
-export default rootReducer;
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export default persistedReducer;
